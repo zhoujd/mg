29322d4789b8718622edea1078be72b1295b8466
Zachary Zhou <zchrzhou@gmail.com>
2024-12-01 21:10:53 +0800

    Add -h option on usage

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ extern void     closetags(void);
 static __dead void
 usage(void)
 {
-	fprintf(stderr, "usage: %s [-BR] [-b file] [-f mode] [-u file] "
+	fprintf(stderr, "usage: %s [-hBR] [-b file] [-f mode] [-u file] "
 	    "[+number] [file ...]\n",
 	    __progname);
 	exit(1);
@@ -87,7 +87,7 @@ main(int argc, char **argv)
 		err(1, "pledge");
 #endif
 
-	while ((o = getopt(argc, argv, "BRb:f:u:")) != -1)
+	while ((o = getopt(argc, argv, "hBRb:f:u:")) != -1)
 		switch (o) {
 		case 'b':
 			batch = 1;
@@ -108,6 +108,7 @@ main(int argc, char **argv)
 		case 'u':
 			conffile = optarg;
 			break;
+		case 'h':
 		default:
 			usage();
 		}
